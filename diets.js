71613badd1515c9cf13888b1d6cbbcb8f2005769
Nida422 <nida987698@gmail.com>
2024-10-20 21:16:05 +0530

    Add files via upload

---
+++ diets.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema=mongoose.Schema;
+const mealPlanSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  mealType: {
+    type: String,
+    required: true,
+  },
+  foodType: {
+    type: [String],  // Array of food types
+    required: true,
+  },
+  foodItems: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Diet = mongoose.model('Diet', mealPlanSchema);
+module.exports = Diet;
---
+++ usermodel.js
@@ -0,0 +1,11 @@
+const passportLocalMongoose=require('passport-local-mongoose');
+const mongoose=require('mongoose');
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    }
+});
+userSchema.plugin(passportLocalMongoose);
+module.exports=mongoose.model('User',userSchema);
